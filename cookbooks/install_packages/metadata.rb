ffe6848655515d9154ea673bc0629f99307805e8
Marcos Arruda <marcos.f.arruda@gmail.com>
2014-02-18 16:05:06 -0300

    code cleaning

--- cookbooks/install_packages/metadata.rb
+++ cookbooks/install_packages/metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures install_packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w{ tomcat maven }.each do |cb|
+%w{ apt tomcat maven }.each do |cb|
   depends cb
 end
 
--- cookbooks/install_packages/recipes/default.rb
+++ cookbooks/install_packages/recipes/default.rb
@@ -7,16 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'apt'
 include_recipe "tomcat::default"
 inlcude_recipe "maven"
 
 execute 'apt-get update'
 
-apt_package "name" do
-  attribute "value" # see attributes section below
-  ...
-  action :action # see actions section below
-end
+
+#apt_package "name" do
+  #attribute "value" # see attributes section below
+  #...
+  #action :action # see actions section below
+#end
 
 #execute 'wget "http://<user>:<pass>@<host>:<port>/manager/deploy?path=/<context>&war=file:<filepath>" -O - -q'
 
