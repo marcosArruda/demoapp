8c03278bfef6ba9992aba661467caec938f52b20
Marcos Arruda <marcos.f.arruda@gmail.com>
2014-02-19 10:50:51 -0300

    Import motd version 0.4.1

---
+++ cookbooks/motd/README.md
@@ -0,0 +1,89 @@
+# Description
+
+Installs message of the day (respects update-motd, if installed)
+
+# Recipes
+
+## default
+
+The default recipe includes the cow and the knife-status recipes
+
+## cow
+
+Displays a friendly warning-cow, logging onto a server in the production environment.
+
+  This is example, a domain.com production server
+
+  just in case you didn't notice the line above, maybe this cow helps:
+
+```
+   ___________________________________
+  < YOU ARE ON A PRODUCTION SERVER! >
+   -----------------------------------
+          \   ^__^
+           \  (oo)\_______
+              (__))\/\    \
+                  ||----w |
+                  ||     ||
+```
+
+## knife-status
+
+plus a knife-status line that displays the knife status of this node. (only when the node supports update-motd)
+
+```
+Last chef run: 10 minutes ago
+```
+
+# Attributes
+
+You can specify whether to use colors in your attributes (defaults to true)
+
+```ruby
+node['motd']['color'] = true
+```
+
+
+# Providers
+
+To use the provider in your cookbook, make sure you put the following line in your metadata.rb
+
+```ruby
+depends 'motd'
+```
+
+## default
+
+The name attribute is only used if update-motd is installed on the system.
+If not, it has no meaning.
+
+Create motd using the shipped cow template
+
+```ruby
+motd '50-mymotd'
+```
+
+Create a motd using a custom template
+
+```ruby
+motd '50-mymotd' do
+  cookbook 'my cookbook'
+  source   'test.erb'
+end
+```
+
+Use the default cow template, but do not use colors
+
+```ruby
+motd '50-mymotd' do
+  color false
+end
+```
+
+Remove a motd (if update-motd is used only)
+
+```ruby
+motd '50-mymotd' do
+  action :delete
+end
+```
---
+++ cookbooks/motd/recipes/knife-status.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: motd
+# Recipe:: default
+#
+# Copyright 2012, Chris Aumann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Chef::Config[:interval] somehow is nil, therefore falling back to
+# configuration of chef_client cookbook (if available)
+# and finally the chef default of 1800s
+interval = Chef::Config[:interval]
+interval ||= node['chef_client']['interval'] if node.attribute?('chef_client')
+interval ||= 1800
+
+# We need to current interval in minutes
+interval = interval / 60
+
+motd '98-knife-status' do
+  source    'knife-status.erb'
+  variables interval: interval,
+            timestamp_file: "#{Chef::Config[:file_cache_path]}/last_successful_chef_run"
+
+  only_if { ::File.directory? '/etc/update-motd.d' }
+end
+
+# add a chef-handler that creates a file with the current timestamp on a successful chef-run
+directory '/var/chef/handlers' do
+  mode 00755
+end
+
+template '/var/chef/handlers/knife_status.rb' do
+  mode   00644
+  source 'knife-status-handler.rb'
+end
+
+chef_handler 'Motd::KnifeStatus' do
+  source '/var/chef/handlers/knife_status.rb'
+  action :enable
+end
